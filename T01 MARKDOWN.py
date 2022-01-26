4e8c3c9cb1c5a3ea7bdb19dfa742433947e3a141
CharlySGG <97917243+CharlySGG@users.noreply.github.com>
2022-01-25 20:52:10 -0600

    T01 MARKDOWN
    Simulacion matematica - Tarea 1

---
+++ T01 MARKDOWN.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # T01 MARKDOWN
+# 
+# ## INSTRUCCION
+# 
+# 1. Instalar Git y crear una cuenta de GitHub.
+# 
+# 2. Crear un Jupyter Notebook que contenga una presentación personal y una ligera introducción a un tema cualquiera de tu licenciatura. Usar Markdown.
+# 
+# 3. Subir el Notebook a un repositorio personal que estarás usando durante este semestre.
+# 
+# 4. Entregar el link al notebook dentro de tu repositorio.
+
+# ### Presentacion
+# 
+# Mi nombre es _Carlos Barboza Ochoa_, actualmente estudio el 6°semestre de nanotecnologia en el **ITESO**.
+# 
+# Es mi primera vez trabajando con estos programas por lo que aun me falta aprender mucho pero espero familiarizarme pronto.
+# 
+# ### Jupyter Notebook
+# 
+# Los "Markdown" utilizados en este trabajo fueron
+# 
+# - Titulos
+# 
+# - Subtitulos
+# 
+# - Letras en negrita
+# 
+# - Letras en cursiva
+# 
+# - Imagenes
+# 
+# **Nota**: Por lo visto para hacer que Notebook te reconozca un _markdown_ es necesario dejar un espacio luego de poner la "sintaxis" necesaria, ademas para que reconozca dos oraciones como lineas distintas se debe dejar un doble espacio entre ellas. Para "presentarlo" se deben picar las teclas _Ctrl + Enter_.
+# 
+# **Consejo**: Jupyter Notebook utiliza muchos _shortcuts_ por lo que vale la pena aprovecharlos.
+# 
+# <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fempresas.blogthinkbig.com%2Fpython-para-todos-2-jupyternotebook%2F&psig=AOvVaw1rOtfc_MYrKjm1fKI1S5_F&ust=1643251709481000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDpg-ezzvUCFQAAAAAdAAAAABAO">
